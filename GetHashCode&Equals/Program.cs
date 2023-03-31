882129fcb769064aa24a5d8c2652757075901b1c
Raphael Duarte <raphaelduartessph@gmail.com>
2023-03-31 12:25:56 -0300

    Add project files.

---
+++ GetHashCode&Equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using GetHashCode_Equals.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
